33265adba2ee0459c2aec258da8c2e647af5e3d2
Michael Mui <michael.mui09@gmail.com>
2025-07-22 15:19:02 -0700

    Tested Symptoms for ONE user

--- backend/symptom_checker_app/serializers.py
+++ backend/symptom_checker_app/serializers.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 from .models import SymptomLog
 
 class SymptomSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    
     class Meta:
         model = SymptomLog
-        fields = ["user", "type", "severity", "frequency", "timestamp"]
+        fields = ["id", "user", "type", "severity", "frequency", "timestamp"]
 
         
