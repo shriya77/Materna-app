dc58ba509171497278ab437aa70f6f4ae6b29da6
Bhavani Potdar <67600962+bhavanipot@users.noreply.github.com>
2025-08-18 20:12:16 -0400

    Update settings.py

--- backend/Materna/settings.py
+++ backend/Materna/settings.py
@@ -145,3 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = False
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5175"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5175"]
